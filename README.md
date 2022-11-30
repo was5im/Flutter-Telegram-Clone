60f81436762b796d1ad2c951ce76a85a94b36f3c
wassim <mohamedbehiani15@gmail.com>
2022-11-30 04:12:21 +0100

    First Commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Twitch Clone App UI
+
+- [Support me on Patreon](https://www.patreon.com/sopheamenvan?fan_landing=true)
+- [Watch on youtube](https://youtu.be/erbFu21T3l8)
+
+![Telegram App UI](https://user-images.githubusercontent.com/16510597/174607709-57f40999-0d68-4245-90be-7d3b99c89ef4.jpeg)
+
+A new Flutter project.
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
+
+For help getting started with Flutter, view our
+[online documentation](https://flutter.dev/docs), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
---
+++ lib/json/chat_json.dart
@@ -0,0 +1,140 @@
+
+const List profile = [
+  {
+    "name": "Mohamed Wassim",
+    "img":
+        "https://yt3.ggpht.com/ytc/AKedOLTHDJOiJgIkQBX7zkuGEXTY0SgIdiikKIAhXmsttw=s600-c-k-c0x00ffffff-no-rj-rp-mo"
+  }
+];
+
+const List chat_data = [
+  {
+    "img":
+        "https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTB8fHByb2ZpbGV8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+    "name": "Oussama Laraba",
+    "text": "Yes, got it. Thanks for your contacting.",
+    "date": "3:23 PM",
+    "badge" : 0
+  },
+  {
+    "img":
+        "https://images.unsplash.com/photo-1603415526960-f7e0328c63b1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDV8fHByb2ZpbGV8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
+    "name": "Abdou Lounis",
+    "text": "Hey man, let's catup soon.",
+    "date": "3:23 PM",
+    "badge" : 3
+  },
+  {
+    "img":
+        "https://images.unsplash.com/photo-1520155707862-5b32817388d6?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTF8fHByb2ZpbGV8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+    "name": "Faycal",
+    "text": "going to sleep now",
+    "date": "2:28 PM",
+    "badge" : 1
+  },
+  {
+    "img":
+        "https://images.unsplash.com/photo-1521572267360-ee0c2909d518?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTd8fHByb2ZpbGV8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+    "name": "Amine",
+    "text": "Have a nice day, Sopheamen 😁",
+    "date": "9:36 PM",
+    "badge" : 1
+  },
+  {
+    "img":
+        "https://images.unsplash.com/photo-1517070208541-6ddc4d3efbcb?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTZ8fHByb2ZpbGV8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+    "name": "Ryan",
+    "text": "How is everthing?",
+    "date": "Sat",
+    "badge" : 0
+  },
+  {
+    "img":
+        "https://images.unsplash.com/photo-1525879000488-bff3b1c387cf?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MjV8fHByb2ZpbGV8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+    "name": "Khadidja",
+    "text": "ok",
+    "date": "Fri",
+    "badge" : 2
+  },
+  {
+    "img":
+        "https://images.unsplash.com/photo-1502823403499-6ccfcf4fb453?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MzV8fHByb2ZpbGV8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+    "name": "Aicha",
+    "text": "take care! speak soon :)",
+    "date": "Thu",
+    "badge" : 0
+  }
+];
+
+
+
+const List messages = [
+  {
+    "isMe": true,
+    "message": "Hey friend, How are you doing?",
+    "time": "1:40 PM",
+    "isLast": false,
+  },
+  {
+    "isMe": true,
+    "message": "good to know you are well.",
+    "time": "1:40 PM",
+    "isLast": false,
+  },
+  {
+    "isMe": true,
+    "message": "Are you free?",
+    "time": "1:41 PM",
+    "isLast": false,
+  },
+  {
+    "isMe": true,
+    "message": "Tomorrow morning",
+    "time": "1:41 PM",
+    "isLast": true,
+  },
+  {
+    "isMe": false,
+    "message": "Hey mate",
+    "time": "2:42 PM",
+    "isLast": false,
+  },
+  
+  {
+    "isMe": false,
+    "message": "good good, how u doing ?",
+    "time": "2:45 PM",
+    "isLast": false,
+  },
+  {
+    "isMe": false,
+    "message": "Ya im here",
+    "time": "2:46 PM",
+    "isLast": true,
+  },
+  {
+    "isMe": true,
+    "message": "perfect",
+    "time": "1:40 PM",
+    "isLast": false,
+  },
+  {
+    "isMe": true,
+    "message": "i'll keep u in touch tho",
+    "time": "1:40 PM",
+    "isLast": false,
+  },
+  {
+    "isMe": true,
+    "message": "please keep ur phone unmute",
+    "time": "1:41 PM",
+    "isLast": false,
+  },
+  {
+    "isMe": true,
+    "message": "see u tomorrow",
+    "time": "1:41 PM",
+    "isLast": true,
+  },
+ 
+];
